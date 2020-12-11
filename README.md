cd8f836ff05297c2bde335b0a7aa48e6b4060e8c
Andrii Koval <a.koval1996@gmail.com>
2020-12-11 14:47:17 +0100

    initial commit :)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ws2020-group-2
 
+## Flask api
+
+start server using `bash start_server.sh`
---
+++ start_server.sh
@@ -0,0 +1,4 @@
+export FLASK_APP=api/api.py
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+flask run
